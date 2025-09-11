39b398e0ec2ec9aeebdda6ba544b20ea7571de2a
nkorkin97 <korkinnikita2208@gmail.com>
2025-09-11 08:45:44 +0300

    1

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,12 +1,22 @@
 import base.BaseTest;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
+import pages.TestPage;
+
+import static java.lang.Thread.sleep;
+import static managers.DriverManager.getDriver;
 
 public class Test extends BaseTest {
 
 
+
     @org.testng.annotations.Test
     void test() throws InterruptedException {
-        Assert.assertTrue(driver.getCurrentUrl().contains("demoqa"));
+        TestPage testPage = new TestPage();
+        PageFactory.initElements(getDriver(), testPage);
+//        Assert.assertTrue(getDriver().getCurrentUrl().contains("demoqa"));
+        testPage.clickElementCard();
+        sleep(1000);
     }
 }
